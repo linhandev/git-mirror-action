2c866815a18d8224eb6650ee46737f89d8479a15
linhandev <hanlin21@huawei.com>
2025-06-27 22:30:10 +0800

    feat: cahnge to only force push certain branches
    Signed-off-by: linhandev <hanlin21@huawei.com>

--- git-mirror.sh
+++ git-mirror.sh
@@ -5,24 +5,34 @@ set -e
 SOURCE_REPO=$1
 DESTINATION_REPO=$2
 SOURCE_DIR=$(basename "$SOURCE_REPO")
-DRY_RUN=$3
-
+BRANCHES=$3
 GIT_SSH_COMMAND="ssh -v"
 
 echo "SOURCE=$SOURCE_REPO"
 echo "DESTINATION=$DESTINATION_REPO"
-echo "DRY RUN=$DRY_RUN"
+echo "BRANCHES=$BRANCHES"
 
-git clone --mirror "$SOURCE_REPO" "$SOURCE_DIR" && cd "$SOURCE_DIR"
-git remote set-url --push origin "$DESTINATION_REPO"
-git fetch -p origin
-# Exclude refs created by GitHub for pull request.
-git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
-
-if [ "$DRY_RUN" = "true" ]
-then
-    echo "INFO: Dry Run, no data is pushed"
-    git push --mirror --dry-run
-else
-    git push --mirror
+if [ -z "$BRANCHES" ]; then
+    echo "No branches specified in BRANCHES."
+    exit 1
 fi
+
+git clone "$SOURCE_REPO" "$SOURCE_DIR" && cd "$SOURCE_DIR"
+git remote set-url --push origin "$DESTINATION_REPO"
+
+OLD_IFS="$IFS"
+IFS=','
+set -- $BRANCHES
+for BR; do
+    echo "Pushing branch: $BR"
+    if ! git switch "$BR"; then
+        echo "Failed to switch to $BR."
+        exit 1
+    fi
+
+    if ! git push --no-verify --force; then
+        echo "Failed to push branch: $BR"
+        exit 1
+    fi
+done
+IFS="$OLD_IFS"
